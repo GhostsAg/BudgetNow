1998bdcb471b007f49f6b082f9402748d152f2ec
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-03-27 21:22:41 -0500

    localdb.js file for local db.

--- public/localdb.js
+++ public/localdb.js
@@ -16,36 +16,35 @@ request.onsuccess = function(event) {
 };
 
 function checkDatabase() {
-  // open a transaction on your pending db
-  db = request.result;
-  // access your pending object store
-  const transaction = db.transaction(["pending"], "readwrite");
-  // get all records from store and set to a variable
-  const store = transaction.objectStore("pending");
-  const getAll = store.getAll();
-  
-  getAll.onsuccess = function() {
-    if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
+    // db = event.request.result;
+    // access your pending object store
+    const transaction = db.transaction(["pending"], "readwrite");
+    // get all records from store and set to a variable
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+    
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(() => {
+            // if successful, open a transaction on your pending db
+            // transaction.open(true)
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+            // access your pending object store
+            // clear all items in your store
+        });
         }
-      })
-      .then(response => response.json())
-      .then(() => {
-          // if successful, open a transaction on your pending db
-          // transaction.open(true)
-          const transaction = db.transaction(["pending"], "readwrite");
-          const store = transaction.objectStore("pending");
-          store.clear();
-          // access your pending object store
-          // clear all items in your store
-      });
-    }
-  };
+    };
 }
 
 request.onerror = function(event) {
@@ -57,7 +56,7 @@ function saveRecord(record) {
     // create a transaction on the pending db with readwrite access
     // access your pending object store
     // add record to your store with add method.
-    const db = request.result;
+    // const db = request.result;
     const transaction = db.transaction(["pending"], "readwrite");
     const pendingStore = transaction.objectStore("pending");
     pendingStore.add(record);
